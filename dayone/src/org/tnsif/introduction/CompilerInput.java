48aed57b10f153473c800e5819d6b57f34fffd30
Pratik Gavali <gavalipratik2@gmail.com>
2023-07-20 14:08:48 +0530

    Commit one

---
+++ dayone/src/org/tnsif/introduction/CompilerInput.java
@@ -0,0 +1,13 @@
+//Program to demonstrate program on compiletime input
+package org.tnsif.introduction;
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short num = 34;
+		System.out.println("The number is " +num);
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/introduction/user_input_DEMO.java
@@ -0,0 +1,18 @@
+//Program to demonstrate user input
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class user_input_DEMO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your Name : ");
+		String S = sc.nextLine();
+		System.out.println("String is : "+S);
+		
+
+	}
+
+}
